98765001802a2b3b3356c5c8462d64360fef496e
Pankajpatel19 <pankajpatelinter@gmail.com>
2025-10-25 13:23:14 +0530

    Enable Source Map

--- FrontEnd/Admin/vite.config.js
+++ FrontEnd/Admin/vite.config.js
@@ -4,19 +4,19 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
-	 build: {
-    sourcemap: true 
-  }
-	server: {
-		port: 5173,
-		proxy: {
-			"/api": {
-				target: "http://localhost:1916",
-				changeOrigin: true,
-				secure: false,
-				rewrite: (path) => path.replace(/^\/api/, ""),
-			},
-		},
-	},
+  plugins: [react(), tailwindcss()],
+  build: {
+    sourcemap: true,
+  },
+  server: {
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "http://localhost:1916",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });

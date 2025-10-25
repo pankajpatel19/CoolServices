6e4ed84fc511b3b8d22a343401b0ac5912e9cfd9
Pankajpatel19 <pankajpatelinter@gmail.com>
2025-10-25 14:58:04 +0530

    remove

--- Backend/app.js
+++ Backend/app.js
@@ -19,7 +19,7 @@ app.use(cookie());
 const cors = require("cors");
 app.use(
   cors({
-    origin: "https://cool-services.vercel.app/",
+    origin: "https://cool-services.vercel.app",
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],

141bc4690bca7212bd12efcaa902738b6d6ce03a
Pankajpatel19 <pankajpatelinter@gmail.com>
2025-11-01 19:13:48 +0530

    change redis config

--- Backend/Controller/Complain/complain.controller.js
+++ Backend/Controller/Complain/complain.controller.js
@@ -19,6 +19,7 @@ export const SubmitComplaints = async (req, res) => {
 export const ShowComplaints = async (req, res) => {
   try {
     const redisComplaints = await redisCLient.get("complaints");
+    console.log(redisComplaints);
 
     if (redisComplaints) {
       return res.status(200).json(JSON.parse(redisComplaints));
--- Backend/config/redis.config.js
+++ Backend/config/redis.config.js
@@ -6,7 +6,7 @@ const client = createClient({
   username: "default",
   password: process.env.REDIS_PASS,
   socket: {
-    host: process.env.REDIS_URL,
+    host: process.env.REDIS_HOST,
     port: 16813,
   },
 });

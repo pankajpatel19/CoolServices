efc4b3c608c6a97c73d1bbcc80fe97db787c8867
Pankajpatel19 <pankajpatelinter@gmail.com>
2025-11-10 19:27:27 +0530

    add width styling to input in login.jsx

--- FrontEnd/Admin/src/Components/User/Login.jsx
+++ FrontEnd/Admin/src/Components/User/Login.jsx
@@ -117,7 +117,7 @@ function Login() {
               min={10}
               value={form.name}
               onChange={handlechange}
-              className="w-100 px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition-colors"
+              className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition-colors"
             />
           </div>
 

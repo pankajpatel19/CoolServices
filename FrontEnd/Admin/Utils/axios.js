7a201fe3181efaf80a73aca4226fdaa7001904a3
Pankajpatel19 <pankajpatelinter@gmail.com>
2026-01-19 11:33:40 +0530

    remove comment in axios file

--- FrontEnd/Admin/Utils/axios.js
+++ FrontEnd/Admin/Utils/axios.js
@@ -5,35 +5,26 @@ const api = axios.create({
   withCredentials: true, // for cookies/JWT
 });
 
-// Response Interceptor (Token Refresh ke liye)
 api.interceptors.response.use(
   (response) => {
-    // Success response ko seedha pass kar do
     return response;
   },
   async (error) => {
     const originalRequest = error.config;
 
-    // 401 (Unauthorized) ka matlab hai accessToken expire ho gaya hai
     if (error.response.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
 
       try {
-        // /refresh route ko call karo. Browser apne aap refreshToken cookie bhej dega
         await api.post("/refresh");
 
-        // Server naya accessToken cookie mein set kar dega
-        // Ab original request (jo fail hui thi) dobara bhejo
         return api(originalRequest);
       } catch (refreshError) {
-        // Agar refresh bhi fail ho (refreshToken expire ho gaya hai)
-        // User ko login page par bhej do
-
         window.location.href = "/login";
         return Promise.reject(refreshError);
       }
     }
     return Promise.reject(error);
-  }
+  },
 );
 export default api;
